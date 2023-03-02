70da7fd9775031f47ff1fc9e29eb9bb68d51e41c
Seçkin <secoblgr52@gmail.com>
2023-03-02 17:14:40 +0300

    typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
-function App() {
+const  App:React.FC = () => {
+
+  let name: string = "Seçkin Bulgur";
+  let age: number = 24;
+  let year: number = 2023;
+  let isActive: boolean = true;
+  let month: string | number = 52; 
+
+
+  type types ={
+    year: number,
+    age:number,
+  }
+
+  //interface type 
+
+  interface personType {
+    name: string,
+    surname: string,
+    city: string,
+    phone: string | number,
+    isMarried?: boolean,          // ?  == is a optional value;
+    hobies: Array<string>,
+  }
+
+  //type2 extends type 
+  interface personType2 extends personType {
+    isActive?: boolean
+  }
+ 
+  let person: personType2 = {
+    name: "Seçkin",
+    surname: "Bulgur",
+    city: "Istanbul",
+    phone: 5467355117,
+    isMarried:false,
+    hobies: ["Swim","Travel","Movie"],
+  }
+  console.log(typeof(person.hobies))
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>TypeScript</h1>
+      <span>{name +", "+ age}</span> 
+      <br />
+      <span>{year}</span>
+      {isActive}
+
+      
+      
     </div>
   );
 }
